cf2cede1744487566f47d017580c41315fe1f8b2
Do Nguyen <daniel@zincer.com>
2018-07-01 20:15:02 +0700

    Initial Commit

---
+++ Thien Assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thien Assignment
+//
+//  Created by Do Nguyen on 7/1/18.
+//  Copyright © 2018 Zincer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
